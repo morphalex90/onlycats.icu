2626e86c32b4164062810f541b1a015d9777d2d3
morphalex90 <piero.nanni@gmail.com>
2023-04-16 20:12:51 +0100

    correct site name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => (
     <footer className="footer">
         <div className="footer__container">
-            <div className="footer__logo">Onlycats</div>
+            <div className="footer__logo">OnlyCats</div>
             <div className="footer__copyright">
                 &copy; {new Date().getFullYear()}  &nbsp;<a href="https://www.pieronanni.me" target="_blank" rel="noreferrer">Piero Nanni</a>
             </div>

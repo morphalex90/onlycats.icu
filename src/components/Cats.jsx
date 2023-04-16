2d29518fc6756c24e1dc183d1026dc2af8674548
morphalex90 <piero.nanni@gmail.com>
2023-04-16 20:12:45 +0100

    move cats logic in reusable component

---
+++ src/components/Cats.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+
+export default function Cats({ category = null }) {
+    const [cats, setCats] = useState([]);
+
+    useEffect(() => {
+        console.log(category);
+        getCats();
+    }, [category]);
+
+    const getCats = () => {
+        axios
+            .get('https://api.thecatapi.com/v1/images/search?page=0&limit=30' + (category != null ? '&category_ids=' + category : ''))
+            .then((res) => { setCats(res.data); })
+            .catch((err) => { console.log(err); });
+    }
+
+    return (
+        <>
+            {cats.length > 0 &&
+                <ResponsiveMasonry columnsCountBreakPoints={{ 768: 2, 992: 3, 1200: 4 }}>
+                    <Masonry>
+                        {(cats.map(cat => {
+                            return (
+                                <img key={cat.id} src={cat.url} alt={cat.id} title={cat.id} height={cat.height} width={cat.width} loading="lazy" />
+                            )
+                        }))}
+                    </Masonry>
+                </ResponsiveMasonry>
+            }
+            <button type="button" className="button" style={{ position: 'fixed', left: '50%', bottom: 50, zIndex: 999, transform: 'translateX(-50%)' }} onClick={() => getCats()}>Get more cats</button>
+        </>
+    );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,9 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 import Head from 'next/head';
-import axios from 'axios';
 import Layout from '@/components/Layout';
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import Cats from '@/components/Cats';
 
 export default function Home() {
-	const [cats, setCats] = useState([]);
-
-	useEffect(() => {
-		getCats();
-	}, []);
-
-	const getCats = () => {
-		axios
-			.get('https://api.thecatapi.com/v1/images/search?page=0&limit=30')
-			.then((res) => { setCats(res.data); })
-			.catch((err) => { console.log(err); });
-	}
-
 	return (
 		<>
 			<Head>
@@ -29,21 +15,10 @@ export default function Home() {
 			<Layout>
 				<section className="section">
 					<div className="section__container">
-						{cats.length > 0 &&
-							<ResponsiveMasonry columnsCountBreakPoints={{ 768: 2, 992: 3, 1200: 4 }}>
-								<Masonry>
-									{(cats.map(cat => {
-										return (
-											<img key={cat.id} src={cat.url} alt={cat.id} height={cat.height} width={cat.width} loading="lazy" />
-										)
-									}))}
-								</Masonry>
-							</ResponsiveMasonry>
-						}
+						<Cats />
 					</div>
 				</section>
 
-				<button type="button" className="button" style={{ position: 'fixed', left: '50%', bottom: 50, zIndex: 999, transform: 'translateX(-50%)' }} onClick={() => getCats()}>Get more cats</button>
 			</Layout>
 		</>
 	)

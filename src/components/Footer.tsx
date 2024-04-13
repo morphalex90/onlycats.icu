184b33c714821cfc0ed6c1f6e954360442c8a2c3
morphalex90 <piero.nanni@gmail.com>
2024-04-13 12:01:35 +0100

    correct site name, remove semicolon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="footer">
             <div className="footer__container">
-                <div className="footer__logo">StarWars</div>
+                <div className="footer__logo">OnlyCats</div>
                 <div className="footer__copyright">
                     &copy; {new Date().getFullYear()}  &nbsp;<a href="https://www.pieronanni.me" target="_blank" rel="noreferrer">Piero Nanni </a>
                     - <a href="https://www.buymeacoffee.com/morphalex90" target="_blank" rel="noreferrer">Buy me a 🍺</a>
@@ -10,4 +10,4 @@ export default function Footer() {
             </div>
         </footer>
     )
-};
+}

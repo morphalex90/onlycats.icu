5d59349bc7105897b35fdae2a97741dbf338f1ac
morphalex90 <piero.nanni@gmail.com>
2024-04-13 11:59:06 +0100

    updates, convert to typescript, update rules, add beer link in footer

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import '@/css/index.scss';
+import Context from '@/contex/BreedContext';
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Context>
+      <Component {...pageProps} />;
+    </Context>
+  )
+}

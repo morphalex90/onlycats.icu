555271356b02233fa29c119b48f116392ec2508e
morphalex90 <piero.nanni@gmail.com>
2025-07-25 12:55:09 +0100

    install plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/css/index.scss';
 import Context from '@/contex/BreedContext';
 import type { AppProps } from "next/app";
+import PlausibleProvider from 'next-plausible';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Context>
-      <Component {...pageProps} />
-    </Context>
+    <PlausibleProvider domain="onlycats.icu" selfHosted={true} trackOutboundLinks={true} scriptProps={{ src: 'https://plausible.morpheus90.com/js/script.js' }}>
+      <Context>
+        <Component {...pageProps} />
+      </Context>
+    </PlausibleProvider>
   )
 }
